655630b81d0ddea856ed36a4f00d7a05c89e2bdf
stnkl <stain.sr@gmail.com>
2019-09-06 13:55:37 +0200

    Only start webserver and OTA in STA mode

--- ESPEssentials.cpp
+++ ESPEssentials.cpp
@@ -12,7 +12,9 @@ void initESPEssentials(String projectName)
 	Serial.begin(115200);
 	Serial.println("");
 
-	Wifi.autoConnect((projectName + " Setup").c_str());
-	WebServer.init();
-	OTA.init((projectName + " (PW: okay)").c_str(), "okay");
+	if(Wifi.autoConnect((projectName + " Setup").c_str()))
+	{
+		WebServer.init();
+		OTA.init((projectName + " (PW: okay)").c_str(), "okay");
+	}
 }
