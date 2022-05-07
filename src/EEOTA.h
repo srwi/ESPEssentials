89cad19f7ff9ce5ffb69a441b9746daee1385a34
stnkl <stnkl@users.noreply.github.com>
2022-05-07 20:51:58 +0200

    Add isBusy method for OTA class

--- src/EEOTA.h
+++ src/EEOTA.h
@@ -9,6 +9,10 @@ class OTAClass : public ArduinoOTAClass
 {
 	public:
 		void init(char const *hostname = "", char const *password = "", uint16_t port = 0);
+		bool isBusy();
+
+	private:
+		bool m_isBusy;
 };
 
 extern OTAClass OTA;
